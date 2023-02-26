db1af89faba1dc1f3f504be66d6837d498278f67
srinivas33333 <91671625+srinivas33333@users.noreply.github.com>
2023-02-27 00:01:14 +0530

    Add files via upload

---
+++ Programming-assignment-1.py
@@ -0,0 +1,33 @@
+### 1. Write a Python program to print "Hello Python"?
+import random
+
+
+print('hello python')
+
+### 2. Write a Python program to do arithmetical operations addition and division.?
+##### Sol:
+a = 10
+b= 3
+add = a+b
+div = a/b
+print(add)
+print(div)
+
+### 3. Write a Python program to find the area of a triangle?
+
+h = 2
+b =3
+triangleArea = 1/2*a*b
+
+### 4. Write a Python program to swap two variables?
+a = 1
+b = 2
+swapHelper = a
+a = b
+b = swapHelper
+print(a, b ,'--swapped')
+
+
+### 5. Write a Python program to generate a random number?
+
+print(random.random())
